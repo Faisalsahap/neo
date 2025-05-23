6bb5ed5fbc8fe582da39e78a19e56c45b3fa0bdb
faisalsahap <faisalsahap@gmail.com>
2025-05-23 10:54:50 +0530

    some

---
+++ abst1.java
@@ -0,0 +1,15 @@
+package com.neocuz.mypackage;
+
+public abstract class abst1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void grow(){
+        System.out.println("Child is Growing");
+    }
+
+    public abstract void study();
+
+
+}
