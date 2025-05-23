e3574d20df6b5dfc9ea9d3d9685ef404efd85fa1
faisalsahap <faisalsahap@gmail.com>
2025-05-23 10:51:30 +0530

    new commit

---
+++ org.java
@@ -0,0 +1,13 @@
+package com.neocuz.mypackage;
+
+public interface org {
+
+
+    public static int salary=12000;
+    public int hike=10;
+    public void attendOffice();
+    public void applyLeave();
+    public  void receiveSalary();
+
+
+}
